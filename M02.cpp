7d89f9255fe44aab4df3b84f831380fae236b706
gusacow <johnellis0207@gmail.com>
2024-10-15 22:14:11 +0800

    M02.cpp

--- M02.cpp
+++ M02.cpp
@@ -3,9 +3,15 @@
 
 using namespace std;
 
+double get_square(float A, short N){
+    double S;
+    S = ((A * N) * (A / (2 * tan((180/N) * (3.1415 / 180))))) / 2;
+    return S;
+}
+
 int main(){
     setlocale(LC_ALL, "Russia");
-    double S; float A; short N;
+    float A; short N;
     cout << "Введите количество сторон" << endl;
     cin >> N;
     cout << "Введите длину сторон" << endl;
@@ -15,6 +21,6 @@ int main(){
     else if(A <= 0)
         cout << "Не возможно с таким значение длины";
     else
-        cout << "Площадь " << N << " угольника = " << (S = ((A * N) * (A / (2 * tan((180/N) * (3.1415 / 180))))) / 2);
+        cout << "Площадь " << N << " угольника = " << get_square(A, N);
     return 0;
 }
