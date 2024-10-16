c7a9e703817b798a6aece83d11971c3267797198
gusacow <johnellis0207@gmail.com>
2024-10-16 21:44:22 +0800

    S01.cpp

---
+++ S01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a{5}; short b{2}; char c{'A'}; float d{2.5};
+    cout << a + b << endl;
+    cout << b - c << endl;
+    cout << d * a << endl;
+    cout << a / b << endl;
+}
