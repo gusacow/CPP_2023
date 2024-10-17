6d00658abbd602fa2c08d3aee556942613ba7e9b
gusacow <johnellis0207@gmail.com>
2024-10-17 20:02:54 +0800

    S31.cpp

---
+++ S31.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    short a;
+    string numbers[10] = {
+        "Ноль", "Один", "Два", "Три", "Четыре", "Пять", "Шесть", "Семь", "Восемь", "Девять"
+    };
+    cout << "Введите цифру от 0-9" << endl;
+    cin >> a; 
+    if(a<0 || a>9)
+        cout << "Ошибка";
+    else
+        cout << numbers[a];
+}
